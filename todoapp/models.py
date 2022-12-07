3dcce21dd3e40d6e80706b3f1b561c5c33c29b7e
CULShatakshi <shatakshisingh6302@gmail.com>
2022-12-07 13:32:14 +0530

    chnages in page

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 # Create your models here.
 class TodoListItem(models.Model):
     content = models.TextField() 
+    #date=models.DateTimeField()
+    #type=models.CharField(max_length=10, choices= Type_Content ,null=False, blank=False)
